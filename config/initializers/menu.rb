f480ca27a22bae26df631135e8b34d7111631994
blaked <dorian.becker@gadz.org>
2017-04-12 18:36:36 +0200

    :bug: fix cache cleaning

--- config/initializers/menu.rb
+++ config/initializers/menu.rb
@@ -1,3 +1,3 @@
-Rails.cache.clear("menu_items_all") if File.exist?('tmp/cache')
+Rails.cache.clear("menu_items_all") if File.exist?(File.expand_path('tmp/cache',Rails.root))
 
 MENU_CONFIG = YAML.load_file(File.expand_path("config/menu.yml",Rails.root))
