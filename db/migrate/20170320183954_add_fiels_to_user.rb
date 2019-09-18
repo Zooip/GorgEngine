b0474872fd6ec7da5e14efe406e0043de06cc3ab
Colin Darie <colin@darie.eu>
2019-09-18 14:16:12 +0200

    Migrations: inherit from Rails version

--- db/migrate/20170320183954_add_fiels_to_user.rb
+++ db/migrate/20170320183954_add_fiels_to_user.rb
@@ -1,4 +1,4 @@
-class AddFielsToUser < ActiveRecord::Migration
+class AddFielsToUser < ActiveRecord::Migration[4.2]
   def change
     add_column :users, :firstname, :string
     add_column :users, :lastname, :string
--- db/migrate/20170330153723_add_name_to_role.rb
+++ db/migrate/20170330153723_add_name_to_role.rb
@@ -1,4 +1,4 @@
-class AddNameToRole < ActiveRecord::Migration
+class AddNameToRole < ActiveRecord::Migration[4.2]
   def change
     add_column :roles, :name, :string
   end
