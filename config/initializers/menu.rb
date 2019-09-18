af6d23e8360c4e162e6d2d4adf43c7570036e08d
Colin Darie <colin@darie.eu>
2019-09-18 14:52:22 +0200

    MenuItems: fix scope of config constant

--- config/initializers/menu.rb
+++ config/initializers/menu.rb
@@ -1,3 +1,5 @@
 Rails.cache.clear("menu_items_all") if File.exist?(File.expand_path('tmp/cache',Rails.root))
 
-MENU_CONFIG = YAML.load_file(File.expand_path("config/menu.yml",Rails.root))
+class MenuItems
+  MENU_CONFIG = YAML.load_file(Rails.root.join("config", "menu.yml"))
+end
